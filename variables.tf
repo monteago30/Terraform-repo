db7b65bf049c0360e733aa3e9f89cefff88f69d4
monteago30 <30839299+monteago30@users.noreply.github.com>
2020-09-19 16:29:12 -0400

    Variable File

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "region-us-west-1" {
+  type    = string
+  default = "us-west-1"
+}
+
+variable "region-ap-northeast-1" {
+  type    = string
+  default = "ap-northeast-1"
+}
